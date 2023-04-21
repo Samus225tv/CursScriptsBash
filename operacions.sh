41ed5270cdcf2d23bbb2b3739858b52ea2db8665
Samuel <samuelcovas@gmail.com>
2023-04-21 08:25:09 +0200

    First commit (primeros 3 scripts)

---
+++ operacions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#variables són "paraules" que contenen un valor
+NOM="samuel"
+#read sirve para leer información del teclado
+read -p "Introdueix un nom: " NOM
+
+
+
+echo "El teu nom és ${NOM}"
