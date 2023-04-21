d0f6b3bb3c97e0089d19632fc0014e4632a9750d
Samuel <samuelcovas@gmail.com>
2023-04-21 09:17:16 +0200

    Suma hecha con parametros

--- suma.sh
+++ suma.sh
@@ -3,10 +3,6 @@
 #aquest script ha de recollir dos paràmetres els ha 
 #de sumar i ha d'escriure per pantalla "a+b= resultat"
 
-read -p "introdueix el valor de a: " a
-read -p "introdueix el valor de b: " b
 
-echo "Has introduït $a i $b"
-
-SUMA=$((a + b))
-echo "${a}+${b}=${SUMA}"
+SUMA=$((${1} + ${2}))
+echo "${1} + ${2} = ${SUMA}"
