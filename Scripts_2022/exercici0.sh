89b14809f26b146a975585516c35f069e2ee9c29
Samuel <samuelcovas@gmail.com>
2023-05-15 09:51:41 +0200

    Exercici0

---
+++ Scripts_2022/exercici0.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Whoami ens diu amb quin usuari estam executant l'script
+echo "EXERCICIC 0"
+echo "---------------------------------------------------"
+echo "Nom de l'usuari: "
+whoami
+echo 
+
+#Exercici 1
+echo "El meu primer script"
+echo
+
+#Exercici 2
+echo "Avui es dia: $(date)"
+echo
+
+#Exercici 3
+
+
+#Exercici 4
+echo "El contingut del directori actual es: " $(cat ${0})
+echo
